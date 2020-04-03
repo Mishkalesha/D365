e4031bc1563516d9d333e896f6d66f6065abeec6
Turchynyk Oleksii <mishkalesha1994@gmail.com>
2020-04-03 07:53:43 +0300

    add rus version with site

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	// Custom JS
 
 	const hamburgerBtn = document.querySelector('.hamburger--squeeze');
+	const mobileMenu = document.querySelector('.mobile__menu');
 
 	$('.header__slider').owlCarousel({
 		items: 1,
@@ -12,6 +13,30 @@ document.addEventListener('DOMContentLoaded', function() {
 		startCarousel();
 	} else {
 		stopCarousel();
+	}
+	if ($(window).width() < 1080) {
+		startCarouselNews();
+	} else {
+		stopCarouselNews();
+	}
+
+	function startCarouselNews() {
+		$('.news__items').owlCarousel({
+			loop: true,
+			center: true,
+			margin: 10,
+			dots: true,
+			responsive: {
+				320: {
+					items: 1
+				}
+			}
+		});
+	}
+	function stopCarouselNews() {
+		let owl2 = $('.news__items');
+		owl2.trigger('destroy.owl.carousel');
+		owl2.addClass('off');
 	}
 
 	function startCarousel() {
@@ -28,49 +53,32 @@ document.addEventListener('DOMContentLoaded', function() {
 				}
 			}
 		});
-		$('.news__items').owlCarousel({
-			loop: true,
-			center: true,
-			margin: 10,
-			dots: true,
-			responsive: {
-				320: {
-					items: 1
-				},
-				720: {
-					items: 3
-				}
-			}
-		});
 	}
 	function stopCarousel() {
 		let owl = $('.top__items');
-		let owl2 = $('.news__items');
 		owl.trigger('destroy.owl.carousel');
-		owl2.trigger('destroy.owl.carousel');
 		owl.addClass('off');
-		owl2.addClass('off');
 	}
 	$('.feedback__slider').owlCarousel({
 		items: 3,
 		loop: true,
-    margin: 20,
-    responsive:{
-      0:{
-        items:1
-      },
-      568:{
-        items: 2
-      },
-      820:{
-        items: 3
-      }
-    }
+		margin: 20,
+		responsive: {
+			0: {
+				items: 1
+			},
+			568: {
+				items: 2
+			},
+			820: {
+				items: 3
+			}
+		}
 	});
 	//Wow initizlize
 	new WOW().init({
-    scrollContainer: null
-  });
+		scrollContainer: null
+	});
 
 	//nselect
 	$('.nselect-1').nSelect({
@@ -81,6 +89,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	//hamburger
 	let hamburgerIsActive = function() {
 		this.classList.toggle('is-active');
+		if (this.classList.contains('is-active')) {
+			mobileMenu.style.transform = 'translateX(0)';
+		} else {
+			mobileMenu.style.transform = 'translateX(-100%)';
+		}
 	};
 	hamburgerBtn.addEventListener('click', hamburgerIsActive);
+
+	//mobile menu
 });
